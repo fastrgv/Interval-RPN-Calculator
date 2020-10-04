2d1d5cf3077c8644eefa59430ae72174df9b1e44
fastrgv <fastrgv@gmail.com>
2020-10-04 15:37:05 -0700

    Update fixlib.sh

--- intRpn/libs/osx/fixlib.sh
+++ intRpn/libs/osx/fixlib.sh
@@ -1,6 +1,12 @@
-# either line works...perhaps 2nd is preferred:
+# apply this to several OSX library files
+# to allow runtime linking on OSX
+# when gaol-related libs are in ./libs/osx/
 
-#install_name_tool -id "@loader_path/libs/osx/$1" $1
+# use this for
+# libgaol-4.2.1.dylib
+# libgdtoa.0.dylib
+# libultim-2.1.0.dylib
+#
+# libifgaol.dylib
 
 install_name_tool -id "@rpath/$1" $1
-
